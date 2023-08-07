d1334f70dbd87835e9dff1418e7812c941628bc4
Coola <ygtkula16@ga>
2023-08-07 15:25:04 +0300

    continue home fragment design

--- app/src/main/java/com/example/currencytracking/view/HomeFragment.kt
+++ app/src/main/java/com/example/currencytracking/view/HomeFragment.kt
@@ -2,26 +2,23 @@ package com.example.currencytracking.view
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.example.currencytracking.R
+import com.example.currencytracking.databinding.FragmentHomeBinding
+import com.example.currencytracking.viewmodel.HomeViewModel
 
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(R.layout.fragment_home) {
 
+    private lateinit var binding: FragmentHomeBinding
+    private val viewModel: HomeViewModel by viewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-    }
+        binding = FragmentHomeBinding.bind(view)
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
 
--- app/src/main/java/com/example/currencytracking/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/currencytracking/viewmodel/HomeViewModel.kt
@@ -1,6 +1,25 @@
 package com.example.currencytracking.viewmodel
 
 import android.app.Application
+import androidx.lifecycle.viewModelScope
+import com.example.currencytracking.repo.ExchangeRepositoryInterface
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class HomeViewModel(application: Application) : BaseViewModel(application) {
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val repository: ExchangeRepositoryInterface,
+    application: Application
+) : BaseViewModel(application){
+
+    val exchangeData = repository.getExchange()
+
+    // Function to refresh the data
+    fun refreshData() {
+        launch {
+            repository.getExchange()
+        }
+    }
+
 }
