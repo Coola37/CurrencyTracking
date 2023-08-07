88c693a00cba664442785685e59a5367d62e8898
Coola <ygtkula16@ga>
2023-08-07 11:50:11 +0300

    Initial commit

---
+++ app/src/main/java/com/example/currencytracking/viewmodel/ConvertViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.currencytracking.viewmodel
+
+import android.app.Application
+
+class ConvertViewModel(application: Application) : BaseViewModel(application){
+
+
+}
---
+++ app/src/main/java/com/example/currencytracking/viewmodel/HomeViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.currencytracking.viewmodel
+
+class HomeViewModel {
+}
